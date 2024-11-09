2e439b61b313afcfb95e95d9827d7b2b8854bc9e
Kamila <64157493+kamilashi@users.noreply.github.com>
2024-11-09 11:36:01 +0100

    added and tested the perspective compensation script

---
+++ Assets/Perspective Compensator.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Graphics
+{
+    public class SpriteScaler : MonoBehaviour
+    {
+        public Camera referenceCamera;
+        public GameObject unitScaleReference;
+
+        void Start()
+        {
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        [ContextMenu("ScaleToReference")]
+        public void ScaleToReference()
+        {
+            float distanceFromCamera = System.Math.Abs(gameObject.transform.position.z - referenceCamera.gameObject.transform.position.z);
+            float referenceDistanceFromCamera = System.Math.Abs(unitScaleReference.transform.position.z - referenceCamera.gameObject.transform.position.z);
+            float scaleFactor = distanceFromCamera / referenceDistanceFromCamera;
+            gameObject.transform.localScale = unitScaleReference.transform.localScale * scaleFactor;
+        }
+
+        [ContextMenu("SnapToCameraBottom")]
+        public void SnapToCameraBottom()
+        {
+            float distanceFromCamera = System.Math.Abs(gameObject.transform.position.z - referenceCamera.gameObject.transform.position.z);
+            float deltaY = distanceFromCamera * (float)System.Math.Tan(referenceCamera.fieldOfView * 0.5 * (System.Math.PI / 180.0));
+            transform.Translate(0.0f,-deltaY, 0.0f);
+        }
+    }
+}
